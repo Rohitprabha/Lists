c74bcb12987c3c9ce1b7a1f8120382111cad5132
Rohitprabha <67499119+Rohitprabha@users.noreply.github.com>
2021-05-18 22:13:48 +0530

    Add files via upload

---
+++ Sorted List/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+namespace Sorted_List
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SortedList<int, string> numberNames = new SortedList<int, string>();
+            numberNames.Add(3, "Three");
+            numberNames.Add(1, "One");
+            numberNames.Add(2, "Two");
+            numberNames.Add(4, null);
+            numberNames.Add(10, "Ten");
+            numberNames.Add(5, "Five");
+            foreach (KeyValuePair<int, string> kvp in numberNames)
+                Console.WriteLine("key: {0}, value: {1}", kvp.Key, kvp.Value);
+            numberNames.Remove(1);                                                  //removes key 1 pair
+            numberNames.RemoveAt(4);                                                //removes key-value pair from index 0
+            foreach (KeyValuePair<int, string> kvp in numberNames)
+                Console.WriteLine("key: {0}, value: {1}", kvp.Key, kvp.Value);
+        }
+    }
+}
